25de3d142fb03caedda0544b6ecb4cb6cf2f113a
7GrandDad <granddad-7@outlook.com>
2021-09-09 14:48:24 -0600

    New Asset Loader on first execute.

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -55,24 +55,25 @@ local GuiLibrary = loadstring(GetURL("NewGuiLibrary.lua"))()
 
 local function getcustomassetfunc(path)
 	if not isfile(path) then
+		spawn(function()
+			local textlabel = Instance.new("TextLabel")
+			textlabel.Size = UDim2.new(1, 0, 0, 36)
+			textlabel.Text = "Downloading "..path
+			textlabel.BackgroundTransparency = 1
+			textlabel.TextStrokeTransparency = 0
+			textlabel.TextSize = 30
+			textlabel.Font = Enum.Font.SourceSans
+			textlabel.TextColor3 = Color3.new(1, 1, 1)
+			textlabel.Position = UDim2.new(0, 0, 0, -36)
+			textlabel.Parent = GuiLibrary["MainGui"]
+			repeat wait() until isfile(path)
+			textlabel:Remove()
+		end)
 		local req = requestfunc({
 			Url = "https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/"..path:gsub("vape/assets", "assets"),
 			Method = "GET"
 		})
 		writefile(path, req.Body)
-	end
-	if not isfile(path) then
-		local textlabel = Instance.new("TextLabel")
-		textlabel.Size = UDim2.new(1, 0, 0, 36)
-		textlabel.Text = "Downloading "..path
-		textlabel.BackgroundTransparency = 1
-		textlabel.TextStrokeTransparency = 0
-		textlabel.TextSize = 30
-		textlabel.TextColor3 = Color3.new(1, 1, 1)
-		textlabel.Position = UDim2.new(0, 0, 0, -36)
-		textlabel.Parent = GuiLibrary["MainGui"]
-		repeat wait() until isfile(path)
-		textlabel:Remove()
 	end
 	return getasset(path) 
 end
