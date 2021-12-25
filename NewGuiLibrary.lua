f086d0f877ac73c877be82d1d8ff26ffa311be5b
7GrandDad <granddad7-neew@outlook.com>
2021-12-24 22:08:33 -0700

    Update NewGuiLibrary.lua

--- NewGuiLibrary.lua
+++ NewGuiLibrary.lua
@@ -2769,7 +2769,7 @@ if shared.VapeExecuted then
 		children.BackgroundTransparency = 1
 		children.BorderSizePixel = 0
 		children.ScrollBarThickness = 3
-		children.ScrollBarImageTransparency = 0.7
+		children.ScrollBarImageTransparency = 0.2
 		children.ScrollBarImageColor3 = Color3.new(0, 0, 0)
 		children.Size = UDim2.new(1, 0, 1, -45)
 		children.ClipsDescendants = true
