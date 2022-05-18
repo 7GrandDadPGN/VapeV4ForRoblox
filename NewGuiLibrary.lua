3e4faeea41c6c3a7bec87c0fef974b9b78d4ef65
7GrandDad <granddad7-neew@outlook.com>
2022-05-18 15:47:17 -0600

    faster load times

--- NewGuiLibrary.lua
+++ NewGuiLibrary.lua
@@ -574,7 +574,9 @@ if shared.VapeExecuted then
 				if v["Type"] == "OptionsButton" and api["findObjectInTable"](api["ObjectsThatCanBeSaved"], i) then
 					if v["Enabled"] then
 						--print(api["ObjectsThatCanBeSaved"][i]["Api"]["ToggleButton"], i, v["Enabled"])
+						local time = tick()
 						api["ObjectsThatCanBeSaved"][i]["Api"]["ToggleButton"](false)
+						print('Loaded '..i..' in '..tick() - time)
 					end
 					if v["Keybind"] ~= "" then
 						api["ObjectsThatCanBeSaved"][i]["Api"]["SetKeybind"](v["Keybind"])
