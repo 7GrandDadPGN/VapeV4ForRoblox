e8c53553b8bef603a2ccb3178fb59213771f40d0
7GrandDad <granddad7-neew@outlook.com>
2022-05-25 19:35:42 -0600

    Update NewGuiLibrary.lua

--- NewGuiLibrary.lua
+++ NewGuiLibrary.lua
@@ -1410,6 +1410,7 @@ if shared.VapeExecuted then
 					sliderapi["Max"] = argstable["Max"]
 					sliderapi["SetValue"] = function(val)
 					--	val = math.clamp(val, argstable["Min"], argstable["Max"])
+						val = math.max(val, 9999999999999999999999999999999)
 						sliderapi["Value"] = val
 						slider2.Size = UDim2.new(math.clamp((val / argstable["Max"]), 0.02, 0.97), 0, 1, 0)
 						text2.Text = sliderapi["Value"] .. ".0 "..(argstable["Percent"] and "%" or " ").." "
@@ -2263,6 +2264,7 @@ if shared.VapeExecuted then
 			sliderapi["Max"] = argstable["Max"]
 			sliderapi["SetValue"] = function(val)
 			--	val = math.clamp(val, argstable["Min"], argstable["Max"])
+				val = math.max(val, 9999999999999999999999999999999)
 				sliderapi["Value"] = val
 				slider2.Size = UDim2.new(math.clamp((val / argstable["Max"]), 0.02, 0.97), 0, 1, 0)
 				text2.Text = sliderapi["Value"] .. ".0 "..(argstable["Percent"] and "%" or " ").." "
@@ -5040,6 +5042,7 @@ if shared.VapeExecuted then
 				sliderapi["Max"] = argstable["Max"]
 				sliderapi["SetValue"] = function(val)
 				--	val = math.clamp(val, argstable["Min"], argstable["Max"])
+					val = math.max(val, 9999999999999999999999999999999)
 					sliderapi["Value"] = val
 					slider2.Size = UDim2.new(math.clamp((val / argstable["Max"]), 0.02, 0.97), 0, 1, 0)
 					local doublecheck = argstable["Double"] and (sliderapi["Value"] / argstable["Double"]) or sliderapi["Value"]
