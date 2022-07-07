b5ce25213a675daf4c6f5317696953f4bb8d648c
7GrandDad <granddad7-neew@outlook.com>
2022-07-07 10:38:45 -0600

    Update NewGuiLibrary.lua

--- NewGuiLibrary.lua
+++ NewGuiLibrary.lua
@@ -110,7 +110,7 @@ if shared.VapeExecuted then
 		local gui = Instance.new("ScreenGui")
 		gui.Name = randomString()
 		gui.DisplayOrder = 999
-		gui.Parent = gethui()
+		gui.Parent = game:GetService("CoreGui")
 	elseif not is_sirhurt_closure and syn and syn.protect_gui then
 		local gui = Instance.new("ScreenGui")
 		gui.Name = randomString()
