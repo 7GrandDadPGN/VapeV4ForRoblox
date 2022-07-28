a64558229b0109357c4c4ccf9def3f0c987c0ca1
7GrandDad <granddad7-neew@outlook.com>
2022-07-27 19:05:52 -0600

    fixed krnl

--- NewGuiLibrary.lua
+++ NewGuiLibrary.lua
@@ -106,13 +106,7 @@ if shared.VapeExecuted then
 		game.Loaded:Wait()
 	end
 
-	if gethui then
-		local gui = Instance.new("ScreenGui")
-		gui.Name = randomString()
-		gui.DisplayOrder = 999
-		gui.Parent = gethui()
-		api["MainGui"] = gui
-	elseif not is_sirhurt_closure and syn and syn.protect_gui then
+	if not is_sirhurt_closure and syn and syn.protect_gui then
 		local gui = Instance.new("ScreenGui")
 		gui.Name = randomString()
 		gui.DisplayOrder = 999
