1c239c9b94273da0dfa545b60f6cafcc54eaf0cd
7GrandDad <grand7dadd@outlook.com>
2023-01-23 12:27:54 -0700

    Update 11156779721.lua

--- CustomModules/11156779721.lua
+++ CustomModules/11156779721.lua
@@ -364,21 +364,20 @@ task.spawn(function()
 	until uninjectflag
 end)
 
-local olddebuginfo = getrenv().debug.info
-setreadonly(getrenv().debug, false)
-getrenv().debug.info = function(level, inf, ...)
-	if level > 6 and level < 10 and inf == "s" and res == nil then 
-		return lplr.PlayerScripts.client.tools.Tool.Collector:GetFullName()
-	end
-	return olddebuginfo(level, inf, ...)
+if not shared.vapehooked then
+	shared.vapehooked = true
+	local tab = {31, 14, 1}
+	local bit_lshift
+	bit_lshift = hookfunction(getrenv().bit32.lshift, newcclosure(function(a, b, ...)
+		if a == 1 and table.find(tab, b) and debug.info(5, "s"):match("FiOne") then 
+			a = 0    
+		end
+		return bit_lshift(a, b, ...)
+	end))
 end
-setreadonly(getrenv().debug, true)
 
 GuiLibrary["SelfDestructEvent"].Event:Connect(function()
 	uninjectflag = true
-	setreadonly(getrenv().debug, false)
-	getrenv().debug.info = olddebuginfo
-	setreadonly(getrenv().debug, true)
 	for i3,v3 in pairs(connectionstodisconnect) do
 		if v3.Disconnect then pcall(function() v3:Disconnect() end) continue end
 		if v3.disconnect then pcall(function() v3:disconnect() end) continue end
