516328ea5a256b90af3e610e74204940c6390e74
7GrandDad <grand7dadd@outlook.com>
2023-02-06 11:18:31 -0700

    Update entityHandler.lua

--- Libraries/entityHandler.lua
+++ Libraries/entityHandler.lua
@@ -70,7 +70,12 @@ do
         local tableIndex, ent = entity.getEntityFromPlayer(obj)
         if tableIndex then
             entity.entityRemovedEvent:Fire(obj)
-            for i,v in next, (ent.Connections or {}) do if v.Disconnect then v:Disconnect() end end
+            if ent.Connections then
+                for i,v in next, ent.Connections do 
+                    if v.Disconnect then pcall(function() v:Disconnect() end) continue end
+                    if v.disconnect then pcall(function() v:disconnect() end) continue end
+                end
+            end
             entity.entityList[tableIndex] = nil
         end
     end
@@ -80,11 +85,7 @@ do
         entity.characterAdded(plr, plr.Character, localcheck, true)
     end
 
-    entity.getHealth = function(ent) -- Override this function to get health on games that dont use humanoid.health
-       return ent.Humanoid.Health, ent.Humanoid.MaxHealth
-    end
-
-    entity.getUpdateConnections = function(ent) -- Override this function to update connections on games that dont use humanoid health
+    entity.getUpdateConnections = function(ent)
         local hum = ent.Humanoid
         return {
             hum:GetPropertyChangedSignal("Health"),
@@ -93,14 +94,28 @@ do
     end
 
     entity.characterAdded = function(plr, char, localcheck, refresh)
+        local id = httpservice:GenerateGUID(true)
+        entity.entityIds[plr.Name] = id
         if char then
             task.spawn(function()
-                local id = httpservice:GenerateGUID(true)
-                entity.entityIds[plr.Name] = id
-                local humrootpart = char:WaitForChild("HumanoidRootPart", 10)
-                local head = char:WaitForChild("Head", 10) or humrootpart and {Position = humrootpart.Position + Vector3.new(0, 3, 0), Name = "Head", Size = Vector3.new(1, 1, 1), CFrame = humrootpart.CFrame + Vector3.new(0, 3, 0), Parent = char}
-                local hum = char:WaitForChild("Humanoid", 10) or char:FindFirstChildWhichIsA("Humanoid")
-                if humrootpart and hum and head and entity.entityIds[plr.Name] == id then
+                local humrootpart = char.PrimaryPart or char:FindFirstChild("HumanoidRootPart")
+                if not humrootpart then
+                    for i = 1, 100 do 
+                        humrootpart = char.PrimaryPart or char:FindFirstChild("HumanoidRootPart")
+                        if humrootpart then break end
+                        task.wait(0.01)
+                    end
+                end
+                local head = char:WaitForChild("Head", 10) or humrootpart and setmetatable({Name = "Head", Size = Vector3.new(1, 1, 1), Parent = char}, {__index = function(t, k) 
+                    if k == 'Position' then
+                        return humrootpart.Position + Vector3.new(0, 3, 0)
+                    elseif k == 'CFrame' then 
+                        return humrootpart.CFrame + Vector3.new(0, 3, 0)
+                    end
+                end})
+                local hum = char:FindFirstChildWhichIsA("Humanoid") or char:WaitForChild("Humanoid", 10)
+                if entity.entityIds[plr.Name] ~= id then return end
+                if humrootpart and hum and head then
                     local childremoved
                     local newent
                     if localcheck then
@@ -120,45 +135,28 @@ do
                             Team = plr.Team,
                             Connections = {}
                         }
-                        setmetatable(newent, {
-                            __tostring = function()
-                                return newent.Player.Name or "Entity"
-                            end,
-                            __index = function(t, k) 
-                                if k == 'Health' then 
-                                    return ({entity.getHealth(newent)})[1] or 100 -- Return Health
-                                elseif k == 'MaxHealth' then
-                                    return ({entity.getHealth(newent)})[2] or 100 -- Return MaxHealth
-                                end
-                                return rawget(t, k)
-                            end
-                        })
-                        if entity.entityIds[plr.Name] == id then
-                            for i, v in next, entity.getUpdateConnections(newent) do 
-                                table.insert(newent.Connections, v:Connect(function() 
-                                    entity.entityUpdatedEvent:Fire(newent)
-                                end))
-                            end
-                            table.insert(entity.entityList, newent)
-                            entity.entityAddedEvent:Fire(newent)
+                        for i, v in next, entity.getUpdateConnections(newent) do 
+                            table.insert(newent.Connections, v:Connect(function() 
+                                entity.entityUpdatedEvent:Fire(newent)
+                            end))
                         end
+                        table.insert(entity.entityList, newent)
+                        entity.entityAddedEvent:Fire(newent)
                     end
-                    if entity.entityIds[plr.Name] == id then
-                        childremoved = char.ChildRemoved:Connect(function(part)
-                            if part.Name == "HumanoidRootPart" or part.Name == "Head" or part.Name == "Humanoid" then
-                                childremoved:Disconnect()
-                                if localcheck then
-                                    entity.isAlive = false
-                                else
-                                    entity.removeEntity(plr)
-                                end
+                    childremoved = char.ChildRemoved:Connect(function(part)
+                        if part == humrootpart or part == hum or part == head then
+                            childremoved:Disconnect()
+                            if localcheck then
+                                entity.isAlive = false
+                            else
+                                entity.removeEntity(plr)
                             end
-                        end)
-                        if newent then 
-                            table.insert(newent.Connections, childremoved)
                         end
-                        table.insert(entity.entityConnections, childremoved)
+                    end)
+                    if newent then 
+                        table.insert(newent.Connections, childremoved)
                     end
+                    table.insert(entity.entityConnections, childremoved)
                 end
             end)
         end
@@ -202,11 +200,12 @@ do
     end
 
     entity.selfDestruct = function()
-        for i,v in next, entity.entityIds do entity.entityIds[i] = nil end
-        for i,v in next, entity.entityConnections do if v.Disconnect then v:Disconnect() end end
-        for i,v in next, entity.entityList do 
-            entity.removeEntity(v.Player)
+        for i,v in next, entity.entityConnections do 
+            if v.Disconnect then pcall(function() v:Disconnect() end) continue end
+            if v.disconnect then pcall(function() v:disconnect() end) continue end
         end
+        for i,v in next, entity.entityIds do entity.entityIds[i] = nil end
+        for i,v in next, entity.entityList do entity.removeEntity(v.Player) end
     end
 end
 
