f602a50d56f30f8ac1dbfe1f5085d0bb2ff3ab30
7GrandDad <grand7dadd@outlook.com>
2023-04-21 17:01:32 -0600

    Update 6872265039.lua

--- CustomModules/6872265039.lua
+++ CustomModules/6872265039.lua
@@ -1987,6 +1987,7 @@ runcode(function()
 		table.insert(emo, v.name)
 		emo2[v.name] = i
 	end
+	table.sort(emo, function(a, b) return a:lower() < b:lower() end)
 	SetEmoteList = SetEmote.CreateDropdown({
 		Name = "Emote",
 		List = emo,
@@ -2074,118 +2075,60 @@ runcode(function()
 	})
 end)
 
-spawn(function()
-	local url = "https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/CustomModules/bedwarsdata"
-
+task.spawn(function()
 	local function createannouncement(announcetab)
-		local notifyframereal = Instance.new("TextButton")
-		notifyframereal.AnchorPoint = Vector2.new(0.5, 0)
-		notifyframereal.BackgroundColor3 = announcetab.Error and Color3.fromRGB(235, 87, 87) or Color3.fromRGB(100, 103, 167)
-		notifyframereal.BorderSizePixel = 0
-		notifyframereal.AutoButtonColor = false
-		notifyframereal.Text = ""
-		notifyframereal.Position = UDim2.new(0.5, 0, 0.01, -36)
-		notifyframereal.Size = UDim2.new(0.4, 0, 0, 0)
-		notifyframereal.Parent = GuiLibrary["MainGui"]
-		local notifyframe = Instance.new("Frame")
-		notifyframe.BackgroundTransparency = 1
-		notifyframe.Size = UDim2.new(1, 0, 1, 0)
-		notifyframe.Parent = notifyframereal
-		local notifyframecorner = Instance.new("UICorner")
-		notifyframecorner.CornerRadius = UDim.new(0, 5)
-		notifyframecorner.Parent = notifyframereal
-		local notifyframeaspect = Instance.new("UIAspectRatioConstraint")
-		notifyframeaspect.AspectRatio = 10
-		notifyframeaspect.DominantAxis = Enum.DominantAxis.Height
-		notifyframeaspect.Parent = notifyframereal
-		local notifyframelist = Instance.new("UIListLayout")
-		notifyframelist.SortOrder = Enum.SortOrder.LayoutOrder
-		notifyframelist.FillDirection = Enum.FillDirection.Horizontal
-		notifyframelist.HorizontalAlignment = Enum.HorizontalAlignment.Left
-		notifyframelist.VerticalAlignment = Enum.VerticalAlignment.Center
-		notifyframelist.Parent = notifyframe
-		local notifyframe2 = Instance.new("Frame")
-		notifyframe2.BackgroundTransparency = 1
-		notifyframe2.BorderSizePixel = 0
-		notifyframe2.LayoutOrder = 1
-		notifyframe2.Size = UDim2.new(0.3, 0, 0, 0)
-		notifyframe2.SizeConstraint = Enum.SizeConstraint.RelativeYY
-		notifyframe2.Parent = notifyframe
-		local notifyframesat = Instance.new("ImageLabel")
-		notifyframesat.BackgroundTransparency = 1
-		notifyframesat.BorderSizePixel = 0
-		notifyframesat.Size = UDim2.new(0.7, 0, 0.7, 0)
-		notifyframesat.LayoutOrder = 2
-		notifyframesat.SizeConstraint = Enum.SizeConstraint.RelativeYY
-		notifyframesat.Image = announcetab.Error and "rbxassetid://6768383834" or "rbxassetid://6685538693"
-		notifyframesat.Parent = notifyframe
-		local notifyframe3 = Instance.new("Frame")
-		notifyframe3.BackgroundTransparency = 1
-		notifyframe3.BorderSizePixel = 0
-		notifyframe3.LayoutOrder = 3
-		notifyframe3.Size = UDim2.new(4.1, 0, 0.8, 0)
-		notifyframe3.SizeConstraint = Enum.SizeConstraint.RelativeYY
-		notifyframe3.Parent = notifyframe
-		local notifyframenotifyframelist = Instance.new("UIPadding")
-		notifyframenotifyframelist.PaddingBottom = UDim.new(0.08, 0)
-		notifyframenotifyframelist.PaddingLeft = UDim.new(0.06, 0)
-		notifyframenotifyframelist.PaddingTop = UDim.new(0.08, 0)
-		notifyframenotifyframelist.Parent = notifyframe3
-		local notifyframeaspectnotifyframeaspect = Instance.new("UIListLayout")
-		notifyframeaspectnotifyframeaspect.Parent = notifyframe3
-		notifyframeaspectnotifyframeaspect.VerticalAlignment = Enum.VerticalAlignment.Center
-		local notifyframelistnotifyframeaspect = Instance.new("TextLabel")
-		notifyframelistnotifyframeaspect.BackgroundTransparency = 1
-		notifyframelistnotifyframeaspect.BorderSizePixel = 0
-		notifyframelistnotifyframeaspect.Size = UDim2.new(1, 0, 0.6, 0)
-		notifyframelistnotifyframeaspect.Font = Enum.Font.Roboto
-		notifyframelistnotifyframeaspect.Text = "Vape Announcement"
-		notifyframelistnotifyframeaspect.TextColor3 = Color3.fromRGB(255, 255, 255)
-		notifyframelistnotifyframeaspect.TextScaled = true
-		notifyframelistnotifyframeaspect.TextWrapped = true
-		notifyframelistnotifyframeaspect.TextXAlignment = Enum.TextXAlignment.Left
-		notifyframelistnotifyframeaspect.Parent = notifyframe3
-		local notifyframe2notifyframeaspect = Instance.new("TextLabel")
-		notifyframe2notifyframeaspect.BackgroundTransparency = 1
-		notifyframe2notifyframeaspect.BorderSizePixel = 0
-		notifyframe2notifyframeaspect.Size = UDim2.new(1, 0, 0.4, 0)
-		notifyframe2notifyframeaspect.Font = Enum.Font.Roboto
-		notifyframe2notifyframeaspect.Text = "<b>"..announcetab.Text.."</b>"
-		notifyframe2notifyframeaspect.TextColor3 = Color3.fromRGB(255, 255, 255)
-		notifyframe2notifyframeaspect.TextScaled = true
-		notifyframe2notifyframeaspect.TextWrapped = true
-		notifyframe2notifyframeaspect.RichText = true
-		notifyframe2notifyframeaspect.TextXAlignment = Enum.TextXAlignment.Left
-		notifyframe2notifyframeaspect.Parent = notifyframe3
-		local notifyprogress = Instance.new("Frame")
-		notifyprogress.Parent = notifyframereal
-		notifyprogress.BorderSizePixel = 0
-		notifyprogress.BackgroundColor3 = Color3.new(1, 1, 1)
-		notifyprogress.Position = UDim2.new(0, 0, 1, -3)
-		notifyprogress.Size = UDim2.new(1, 0, 0, 3)
-		local notifyprogresscorner = Instance.new("UICorner")
-		notifyprogresscorner.CornerRadius = UDim.new(0, 100)
-		notifyprogresscorner.Parent = notifyprogress
-		game:GetService("TweenService"):Create(notifyframereal, TweenInfo.new(0.12), {Size = UDim2.fromScale(0.4, 0.065)}):Play()
-		game:GetService("TweenService"):Create(notifyprogress, TweenInfo.new(announcetab.Time or 20, Enum.EasingStyle.Linear), {Size = UDim2.new(0, 0, 0, 3)}):Play()
+		local vapenotifframe = Instance.new("TextButton")
+		vapenotifframe.AnchorPoint = Vector2.new(0.5, 0)
+		vapenotifframe.BackgroundColor3 = Color3.fromRGB(34, 34, 34)
+		vapenotifframe.Size = UDim2.new(1, -10, 0, 50)
+		vapenotifframe.Position = UDim2.new(0.5, 0, 0, -100)
+		vapenotifframe.AutoButtonColor = false
+		vapenotifframe.Text = ""
+		vapenotifframe.Parent = shared.GuiLibrary.MainGui
+		local vapenotifframecorner = Instance.new("UICorner")
+		vapenotifframecorner.CornerRadius = UDim.new(0, 256)
+		vapenotifframecorner.Parent = vapenotifframe
+		local vapeicon = Instance.new("Frame")
+		vapeicon.Size = UDim2.new(0, 40, 0, 40)
+		vapeicon.Position = UDim2.new(0, 5, 0, 5)
+		vapeicon.BackgroundColor3 = Color3.fromRGB(26, 26, 26)
+		vapeicon.Parent = vapenotifframe
+		local vapeiconicon = Instance.new("ImageLabel")
+		vapeiconicon.BackgroundTransparency = 1
+		vapeiconicon.Size = UDim2.new(1, -10, 1, -10)
+		vapeiconicon.AnchorPoint = Vector2.new(0.5, 0.5)
+		vapeiconicon.Position = UDim2.new(0.5, 0, 0.5, 0)
+		vapeiconicon.Image = getsynasset("vape/assets/VapeIcon.png")
+		vapeiconicon.Parent = vapeicon
+		local vapeiconcorner = Instance.new("UICorner")
+		vapeiconcorner.CornerRadius = UDim.new(0, 256)
+		vapeiconcorner.Parent = vapeicon
+		local vapetext = Instance.new("TextLabel")
+		vapetext.Size = UDim2.new(1, -55, 1, -10)
+		vapetext.Position = UDim2.new(0, 50, 0, 5)
+		vapetext.BackgroundTransparency = 1
+		vapetext.TextScaled = true
+		vapetext.RichText = true
+		vapetext.Font = Enum.Font.Ubuntu
+		vapetext.Text = announcetab.Text
+		vapetext.TextColor3 = Color3.new(1, 1, 1)
+		vapetext.TextXAlignment = Enum.TextXAlignment.Left
+		vapetext.Parent = vapenotifframe
+		tweenService:Create(vapenotifframe, TweenInfo.new(0.3), {Position = UDim2.new(0.5, 0, 0, 5)}):Play()
 		local sound = Instance.new("Sound")
 		sound.PlayOnRemove = true
 		sound.SoundId = "rbxassetid://6732495464"
 		sound.Parent = workspace
-		sound:Remove()
-		notifyframereal.MouseButton1Click:Connect(function()
+		sound:Destroy()
+		vapenotifframe.MouseButton1Click:Connect(function()
 			local sound = Instance.new("Sound")
 			sound.PlayOnRemove = true
 			sound.SoundId = "rbxassetid://6732690176"
 			sound.Parent = workspace
-			sound:Remove()
-			notifyframereal:Remove()
-			notifyframereal = nil
+			sound:Destroy()
+			vapenotifframe:Destroy()
 		end)
-		task.wait(announcetab.Time or 20)
-		if notifyframereal then
-			notifyframereal:Remove()
-		end
+		game:GetService("Debris"):AddItem(vapenotifframe, announcetab.Time or 20)
 	end
 
 	local function rundata(datatab, olddatatab)
@@ -2198,7 +2141,7 @@ spawn(function()
 				end))
 				game:GetService("StarterGui"):SetCore("SendNotification", {
 					Title = "Vape",
-					Text = "Vape is currently disabled, check the discord for updates discord.gg/vxpe",
+					Text = "Vape is currently disabled, please use vape later.",
 					Duration = 30,
 				})
 			end
@@ -2206,13 +2149,7 @@ spawn(function()
 				lplr:Kick(datatab.KickUsers[tostring(lplr.UserId)])
 			end
 		else
-			local newdatatab = {}
-			for i,v in pairs(datatab) do 
-				if not olddatatab or olddatatab[i] ~= v then 
-					newdatatab[i] = v
-				end
-			end
-			if newdatatab.Disabled then 
+			if datatab.Disabled then 
 				coroutine.resume(coroutine.create(function()
 					repeat task.wait() until shared.VapeFullyLoaded
 					task.wait(1)
@@ -2220,42 +2157,54 @@ spawn(function()
 				end))
 				game:GetService("StarterGui"):SetCore("SendNotification", {
 					Title = "Vape",
-					Text = "Vape is currently disabled, check the discord for updates discord.gg/vxpe",
+					Text = "Vape is currently disabled, please use vape later.",
 					Duration = 30,
 				})
 			end
 			if datatab.KickUsers and datatab.KickUsers[tostring(lplr.UserId)] then
 				lplr:Kick(datatab.KickUsers[tostring(lplr.UserId)])
 			end
-			if newdatatab.Announcement and newdatatab.Announcement.ExpireTime >= os.time() then 
-				spawn(function()
-					createannouncement(newdatatab.Announcement)
+			if datatab.Announcement and datatab.Announcement.ExpireTime >= os.time() and (datatab.Announcement.ExpireTime ~= olddatatab.Announcement.ExpireTime or datatab.Announcement.Text ~= olddatatab.Announcement.Text) then 
+				task.spawn(function()
+					createannouncement(datatab.Announcement)
 				end)
 			end
 		end
 	end
-
-	pcall(function()
-		if betterisfile("vape/Profiles/bedwarsdata.txt") == false then 
-			writefile("vape/Profiles/bedwarsdata.txt", game:HttpGet(url, true))
-		end
-		local olddata = readfile("vape/Profiles/bedwarsdata.txt")
-		local newdata = game:HttpGet(url, true)
-		if newdata ~= olddata then 
-			rundata(game:GetService("HttpService"):JSONDecode(newdata), game:GetService("HttpService"):JSONDecode(olddata))
-			olddata = newdata
-			writefile("vape/Profiles/bedwarsdata.txt", newdata)
-		else
-			rundata(game:GetService("HttpService"):JSONDecode(olddata))
-		end
-		repeat
-			task.wait(60)
-			newdata = game:HttpGet(url, true)
-			if newdata ~= olddata then 
-				rundata(game:GetService("HttpService"):JSONDecode(newdata), game:GetService("HttpService"):JSONDecode(olddata))
-				olddata = newdata
-				writefile("vape/Profiles/bedwarsdata.txt", newdata)
+	task.spawn(function()
+		pcall(function()
+			if not isfile("vape/Profiles/bedwarsdata.txt") then 
+				local commit = "main"
+				for i,v in pairs(game:HttpGet("https://github.com/7GrandDadPGN/VapeV4ForRoblox"):split("\n")) do 
+					if v:find("commit") and v:find("fragment") then 
+						local str = v:split("/")[5]
+						commit = str:sub(0, str:find('"') - 1)
+						break
+					end
+				end
+				writefile("vape/Profiles/bedwarsdata.txt", game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/"..commit.."/CustomModules/bedwarsdata", true))
 			end
-		until uninjectflag
+			local olddata = readfile("vape/Profiles/bedwarsdata.txt")
+
+			repeat
+				local commit = "main"
+				for i,v in pairs(game:HttpGet("https://github.com/7GrandDadPGN/VapeV4ForRoblox"):split("\n")) do 
+					if v:find("commit") and v:find("fragment") then 
+						local str = v:split("/")[5]
+						commit = str:sub(0, str:find('"') - 1)
+						break
+					end
+				end
+				
+				local newdata = game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/"..commit.."/CustomModules/bedwarsdata", true)
+				if newdata ~= olddata then 
+					rundata(game:GetService("HttpService"):JSONDecode(newdata), game:GetService("HttpService"):JSONDecode(olddata))
+					olddata = newdata
+					writefile("vape/Profiles/bedwarsdata.txt", newdata)
+				end
+
+				task.wait(10)
+			until not vapeInjected
+		end)
 	end)
 end)
