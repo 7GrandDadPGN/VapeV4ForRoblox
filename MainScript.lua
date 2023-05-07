b25de8b26ef78f213fdf899961e9a2d2598d3677
7GrandDad <grand7dadd@outlook.com>
2023-05-07 16:25:24 -0600

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -964,7 +964,7 @@ local function TextGUIUpdate()
                 VapeTextExtra.TextXAlignment = Enum.TextXAlignment.Right
                 VapeTextExtra.Position = UDim2.fromOffset(5, 1)
                 VapeLogo.Position = UDim2.new(1, -142, 0, 8)
-                VapeText.Position = UDim2.new(1, -158, 0, (VapeLogo.Visible and (TextGUIBackgroundToggle.Enabled and 41 or 35) or 5) + (VapeCustomText.Visible and 25 or 0) - (inputService.TouchEnabled and 15 or 23))
+                VapeText.Position = UDim2.new(1, -158, 0, (VapeLogo.Visible and (TextGUIBackgroundToggle.Enabled and 41 or 35) or 5) + (VapeCustomText.Visible and 25 or 0) - (inputService.TouchEnabled and 14 or 23))
                 VapeCustomText.Position = UDim2.fromOffset(0, VapeLogo.Visible and 35 or 0)
                 VapeCustomText.TextXAlignment = Enum.TextXAlignment.Right
                 VapeBackgroundList.HorizontalAlignment = Enum.HorizontalAlignment.Right
@@ -974,7 +974,7 @@ local function TextGUIUpdate()
                 VapeTextExtra.TextXAlignment = Enum.TextXAlignment.Left
                 VapeTextExtra.Position = UDim2.fromOffset(5, 1)
                 VapeLogo.Position = UDim2.fromOffset(2, 8)
-                VapeText.Position = UDim2.fromOffset(6, (VapeLogo.Visible and (TextGUIBackgroundToggle.Enabled and 41 or 35) or 5) + (VapeCustomText.Visible and 25 or 0) - 23)
+                VapeText.Position = UDim2.fromOffset(6, (VapeLogo.Visible and (TextGUIBackgroundToggle.Enabled and 41 or 35) or 5) + (VapeCustomText.Visible and 25 or 0) - (inputService.TouchEnabled and 14 or 23))
 				VapeCustomText.Position = UDim2.fromOffset(0, VapeLogo.Visible and 35 or 0)
 				VapeCustomText.TextXAlignment = Enum.TextXAlignment.Left
                 VapeBackgroundList.HorizontalAlignment = Enum.HorizontalAlignment.Left
