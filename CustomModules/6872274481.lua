e3271ccd02c769beabfc812172c6061eb6ea681e
7GrandDad <grand7dadd@outlook.com>
2023-07-14 18:18:41 -0600

    Update 6872274481.lua

--- CustomModules/6872274481.lua
+++ CustomModules/6872274481.lua
@@ -9973,7 +9973,7 @@ runFunction(function()
 						if item and lplr.Character.HandInvItem.Value == item.tool then 
 							bedwars.ClientHandler:Get("ScytheDash"):SendToServer({direction = Vector3.new(9e9, 9e9, 9e9)})
 							if bedwarsStore.attackReachUpdate < tick() then 
-								lplr.Character:SetAttribute("SpeedBoost", 1.5)
+								lplr.Character:SetAttribute("SpeedBoost", 1)
 							else
 								lplr.Character:SetAttribute("SpeedBoost", nil)
 							end
