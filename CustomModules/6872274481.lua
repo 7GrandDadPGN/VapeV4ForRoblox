938a2b8918bc0537f061bf5591f01899f304389b
7GrandDadPGN <29617380+7GrandDadPGN@users.noreply.github.com>
2024-04-05 23:27:43 -0600

    doodoo

--- CustomModules/6872274481.lua
+++ CustomModules/6872274481.lua
@@ -8794,6 +8794,52 @@ runFunction(function()
 end)
 
 runFunction(function()
+	local Disabler = {Enabled = false}
+
+	Disabler = GuiLibrary.ObjectsThatCanBeSaved.UtilityWindow.Api.CreateOptionsButton({
+		Name = 'Disabler',
+		Function = function(callback)
+			if callback then
+				task.spawn(function()
+					repeat
+						bedwars.Client:Get('RocketImpulse'):CallServer({velocity = Vector3.new(math.huge, math.huge, math.huge)})
+						task.wait(0.1)
+					until (not Disabler.Enabled)
+				end)
+			end
+		end,
+		HoverText = 'funny'
+	})
+end)
+
+runFunction(function()
+	local SetMechanic = {Enabled = false}
+	local SetMechanicList = {Value = 'capture_points'}
+
+	SetMechanic = GuiLibrary.ObjectsThatCanBeSaved.UtilityWindow.Api.CreateOptionsButton({
+		Name = 'SetMechanic',
+		Function = function(callback)
+			if callback and store.matchState == 0 then
+				bedwars.Client:GetNamespace('CustomMatches'):Get('SetMatchMechanic'):CallServerAsync(SetMechanicList.Value)
+			end
+		end,
+		HoverText = 'average bw dev iq'
+	})
+	local list = {}
+	for i, v in require(replicatedStorage.TS.match.mechanics["match-mechanic-types"]).MatchMechanic do table.insert(list, v) end
+	SetMechanicList = SetMechanic.CreateDropdown({
+		Name = 'Mechanic',
+		List = list,
+		Function = function(val)
+			if SetMechanic.Enabled then
+				SetMechanic.ToggleButton(false)
+				SetMechanic.ToggleButton(false)
+			end
+		end
+	})
+end)
+
+runFunction(function()
 	local BowExploit = {Enabled = false}
 	local BowExploitTarget = {Value = "Mouse"}
 	local BowExploitAutoShootFOV = {Value = 1000}
