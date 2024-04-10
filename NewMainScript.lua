f626162dad32e5f6afe2e7dc29aa79721705090a
7GrandDadPGN <29617380+7GrandDadPGN@users.noreply.github.com>
2024-04-10 08:43:47 -0600

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -40,6 +40,10 @@ local function vapeGithubRequest(scripturl)
 		end)
 		suc, res = pcall(function() return game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/"..readfile("vape/commithash.txt").."/"..scripturl, true) end)
 		if not suc or res == "404: Not Found" then
+			if identifyexecutor and ({identifyexecutor()})[1] == 'Wave' then 
+				displayErrorPopup('Stop using detected garbage, Vape will not work on such garabge until they fix BOTH HttpGet & file functions.')
+				error(res)
+			end
 			displayErrorPopup("Failed to connect to github : vape/"..scripturl.." : "..res)
 			error(res)
 		end
