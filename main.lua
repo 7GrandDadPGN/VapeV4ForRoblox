fdfbf11e42a09eeee50e003d22eba35dade7ff67
Melon <80179069+OfficiallyMelon@users.noreply.github.com>
2025-04-21 19:31:37 +1000

    Add Celery to the non functioning functions list.
    😭

--- main.lua
+++ main.lua
@@ -3,7 +3,7 @@ if shared.vape then shared.vape:Uninject() end
 
 -- why do exploits fail to implement anything correctly? Is it really that hard?
 if identifyexecutor then
-	if table.find({'Argon', 'Wave'}, ({identifyexecutor()})[1]) then
+	if table.find({'Argon', 'Wave', 'Celery'}, ({identifyexecutor()})[1]) then
 		getgenv().setthreadidentity = nil
 	end
 end
