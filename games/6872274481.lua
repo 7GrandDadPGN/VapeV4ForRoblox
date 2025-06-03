2138204e3f6284699a254dbe1aa7350719db9914
Trariviste <gifaniga@gmail.com>
2025-06-03 18:45:10 +0200

    Update 6872274481.lua

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -3109,6 +3109,187 @@ run(function()
 	})
 end)
 
+local Fly local LongJump run(function() local Value local VerticalValue local WallCheck local PopBalloons local TP local ProgressBarToggle local rayCheck = RaycastParams.new() rayCheck.RespectCanCollide = true local up, down, old = 0, 0
+
+Fly = vape.Categories.Blatant:CreateModule({
+	Name = 'Fly',
+	Function = function(callback)
+		frictionTable.Fly = callback or nil
+		updateVelocity()
+		if callback then
+			up, down, old = 0, 0, bedwars.BalloonController.deflateBalloon
+			bedwars.BalloonController.deflateBalloon = function() end
+			local tpTick, tpToggle, oldy = tick(), true
+
+			if lplr.Character and (lplr.Character:GetAttribute('InflatedBalloons') or 0) == 0 and getItem('balloon') then
+				bedwars.BalloonController:inflateBalloon()
+			end
+			Fly:Clean(vapeEvents.AttributeChanged.Event:Connect(function(changed)
+				if changed == 'InflatedBalloons' and (lplr.Character:GetAttribute('InflatedBalloons') or 0) == 0 and getItem('balloon') then
+					bedwars.BalloonController:inflateBalloon()
+				end
+			end))
+			Fly:Clean(runService.PreSimulation:Connect(function(dt)
+				if entitylib.isAlive and not InfiniteFly.Enabled and isnetworkowner(entitylib.character.RootPart) then
+					local flyAllowed = (lplr.Character:GetAttribute('InflatedBalloons') and lplr.Character:GetAttribute('InflatedBalloons') > 0) or store.matchState == 2
+					local mass = (1.5 + (flyAllowed and 6 or 0) * (tick() % 0.4 < 0.2 and -1 or 1)) + ((up + down) * VerticalValue.Value)
+					local root, moveDirection = entitylib.character.RootPart, entitylib.character.Humanoid.MoveDirection
+					local velo = getSpeed()
+					local destination = (moveDirection * math.max(Value.Value - velo, 0) * dt)
+					rayCheck.FilterDescendantsInstances = {lplr.Character, gameCamera, AntiFallPart}
+					rayCheck.CollisionGroup = root.CollisionGroup
+
+					if WallCheck.Enabled then
+						local ray = workspace:Raycast(root.Position, destination, rayCheck)
+						if ray then
+							destination = ((ray.Position + ray.Normal) - root.Position)
+						end
+					end
+
+					if not flyAllowed then
+						if tpToggle then
+							local airleft = (tick() - entitylib.character.AirTime)
+							if airleft > 2 then
+								if not oldy then
+									local ray = workspace:Raycast(root.Position, Vector3.new(0, -1000, 0), rayCheck)
+									if ray and TP.Enabled then
+										tpToggle = false
+										oldy = root.Position.Y
+										tpTick = tick() + 0.11
+										root.CFrame = CFrame.lookAlong(Vector3.new(root.Position.X, ray.Position.Y + entitylib.character.HipHeight, root.Position.Z), root.CFrame.LookVector)
+									end
+								end
+							end
+						else
+							if oldy then
+								if tpTick < tick() then
+									local newpos = Vector3.new(root.Position.X, oldy, root.Position.Z)
+									root.CFrame = CFrame.lookAlong(newpos, root.CFrame.LookVector)
+									tpToggle = true
+									oldy = nil
+								else
+									mass = 0
+								end
+							end
+						end
+					end
+
+					root.CFrame += destination
+					root.AssemblyLinearVelocity = (moveDirection * velo) + Vector3.new(0, mass, 0)
+				end
+			end))
+			Fly:Clean(inputService.InputBegan:Connect(function(input)
+				if not inputService:GetFocusedTextBox() then
+					if input.KeyCode == Enum.KeyCode.Space or input.KeyCode == Enum.KeyCode.ButtonA then
+						up = 1
+					elseif input.KeyCode == Enum.KeyCode.LeftShift or input.KeyCode == Enum.KeyCode.ButtonL2 then
+						down = -1
+					end
+				end
+			end))
+			Fly:Clean(inputService.InputEnded:Connect(function(input)
+				if input.KeyCode == Enum.KeyCode.Space or input.KeyCode == Enum.KeyCode.ButtonA then
+					up = 0
+				elseif input.KeyCode == Enum.KeyCode.LeftShift or input.KeyCode == Enum.KeyCode.ButtonL2 then
+					down = 0
+				end
+			end))
+			if inputService.TouchEnabled then
+				pcall(function()
+					local jumpButton = lplr.PlayerGui.TouchGui.TouchControlFrame.JumpButton
+					Fly:Clean(jumpButton:GetPropertyChangedSignal('ImageRectOffset'):Connect(function()
+						up = jumpButton.ImageRectOffset.X == 146 and 1 or 0
+					end))
+				end)
+			end
+
+			if ProgressBarToggle.Enabled then
+				local frame = Instance.new("Frame")
+				frame.AnchorPoint = Vector2.new(0.5, 0)
+				frame.Position = UDim2.new(0.5, 0, 1, -200)
+				frame.Size = UDim2.new(0.2, 0, 0, 20)
+				frame.BackgroundTransparency = 0.5
+				frame.BorderSizePixel = 0
+				frame.BackgroundColor3 = Color3.new(0, 0, 0)
+				frame.Visible = Fly.Enabled
+				frame.Parent = GuiLibrary.MainGui
+
+				local inner = frame:Clone()
+				inner.AnchorPoint = Vector2.new(0, 0)
+				inner.Position = UDim2.new(0, 0, 0, 0)
+				inner.Size = UDim2.new(1, 0, 0, 20)
+				inner.BackgroundTransparency = 0
+				inner.Visible = true
+				inner.Parent = frame
+
+				local label = Instance.new("TextLabel")
+				label.Text = "2s"
+				label.Font = Enum.Font.Gotham
+				label.TextStrokeTransparency = 0
+				label.TextColor3 = Color3.new(0.9, 0.9, 0.9)
+				label.TextSize = 20
+				label.Size = UDim2.new(1, 0, 1, 0)
+				label.BackgroundTransparency = 1
+				label.Position = UDim2.new(0, 0, -1, 0)
+				label.Parent = frame
+			end
+		else
+			bedwars.BalloonController.deflateBalloon = old
+			if PopBalloons.Enabled and entitylib.isAlive and (lplr.Character:GetAttribute('InflatedBalloons') or 0) > 0 then
+				for _ = 1, 3 do
+					bedwars.BalloonController:deflateBalloon()
+				end
+			end
+		end
+	end,
+	ExtraText = function()
+		return 'Heatseeker'
+	end,
+	Tooltip = 'Makes you go zoom.'
+})
+
+Value = Fly:CreateSlider({
+	Name = 'Speed',
+	Min = 1,
+	Max = 23,
+	Default = 23,
+	Suffix = function(val)
+		return val == 1 and 'stud' or 'studs'
+	end
+})
+
+VerticalValue = Fly:CreateSlider({
+	Name = 'Vertical Speed',
+	Min = 1,
+	Max = 150,
+	Default = 50,
+	Suffix = function(val)
+		return val == 1 and 'stud' or 'studs'
+	end
+})
+
+WallCheck = Fly:CreateToggle({
+	Name = 'Wall Check',
+	Default = true
+})
+
+PopBalloons = Fly:CreateToggle({
+	Name = 'Pop Balloons',
+	Default = true
+})
+
+TP = Fly:CreateToggle({
+	Name = 'TP Down',
+	Default = true
+})
+
+ProgressBarToggle = Fly:CreateToggle({
+	Name = 'Progress Bar',
+	Default = true
+})
+
+end)
+																																	
 run(function()
 	local TexturePack
 	local Mode
