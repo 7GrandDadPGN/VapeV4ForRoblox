eb7ded2a03eb6e8cb5c6eea62de3dfe5cc917ed1
7GrandDadPGN <29617380+7GrandDadPGN@users.noreply.github.com>
2026-09-18 21:19:36 -0600

    Added AutoTeam

---
+++ src/games/prison life/155615604 - main/Utility/AutoTeam.lua
@@ -0,0 +1,19 @@
+local AutoTeam
+
+AutoTeam = vape.Categories.Utility:CreateModule({
+	Name = 'AutoTeam',
+	Function = function(callback)
+		if callback then
+			local gui = lplr.PlayerGui:FindFirstChild('TeamsFrame', true)
+			if gui then
+				for _, holder in gui:GetChildren() do
+					if holder.Button.AutoButtonColor then
+						firesignal(holder.Button.MouseButton1Click)
+						break
+					end
+				end
+			end
+		end
+	end,
+	Tooltip = 'Automatically join a team when joining the server'
+})
