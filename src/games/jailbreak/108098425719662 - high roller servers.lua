2ac4bb4db3590eba35a1440c9796c6fab979a2e7
7GrandDadPGN <29617380+7GrandDadPGN@users.noreply.github.com>
2026-09-23 15:12:01 -0600

    Added High Roller Servers

---
+++ src/games/jailbreak/108098425719662 - high roller servers.lua
@@ -0,0 +1,41 @@
+local vape = shared.vape
+local loadstring = function(...)
+	local res, err = loadstring(...)
+	if err and vape then
+		vape:CreateNotification('Vape', 'Failed to load : '..err, 30, 'alert')
+	end
+	return res
+end
+local isfile = isfile or function(file)
+	local suc, res = pcall(function()
+		return readfile(file)
+	end)
+	return suc and res ~= nil and res ~= ''
+end
+local function downloadFile(path, func)
+	if not isfile(path) then
+		local suc, res = pcall(function()
+			return game:HttpGet('https://raw.githubusercontent.com/7GrandDadPGN/VapeCompiled/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
+		end)
+		if not suc or res == '404: Not Found' then
+			error(res)
+		end
+		if path:find('.lua') then
+			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res
+		end
+		writefile(path, res)
+	end
+	return (func or readfile)(path)
+end
+
+vape.Place = 606849621
+if isfile('newvape/games/'..vape.Place..'.lua') then
+	loadstring(readfile('newvape/games/'..vape.Place..'.lua'), 'jailbreak')()
+else
+	if not shared.VapeDeveloper then
+		local success, result = pcall(downloadFile, 'newvape/games/'..vape.Place..'.lua')
+		if success and result then
+			loadstring(result, 'jailbreak')()
+		end
+	end
+end
